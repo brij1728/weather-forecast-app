7622095e63e91825ced533497deffa4cce7a86af
Brijesh Kumar <bk10895@gmail.com>
2024-04-11 20:34:53 +0530

    feat: add temperature unit toggle and wind speed conversion
    - Add toggle for selecting temperature unit (Celsius or Fahrenheit)
    - Update temperature and wind speed display based on selected unit
    - Add function to convert wind speed from km/h to mph
    - Refactor date parsing and formatting

--- src/components/CityWeatherDetails/CityWeatherDetails.tsx
+++ src/components/CityWeatherDetails/CityWeatherDetails.tsx
@@ -1,90 +1,124 @@
 "use client";
 
-import React, { useEffect, useState } from 'react'
-import { convertWindSpeedToKmh, kelvinToCelsius } from '@/utils';
-import { format, parse, parseISO } from 'date-fns';
+import React, { useEffect, useState } from "react";
+import {
+  convertWindSpeedToKmh,
+  convertWindSpeedToMph,
+  kelvinToCelsius,
+  kelvinToFahrenheit,
+} from "@/utils";
+import { format, parseISO } from "date-fns";
 
-import { Spinner } from '../Spinner';
-import { WeatherContainer } from '../WeatherContainer';
-import { WeatherForecastResponse } from '@/types'
-import { fetchWeatherData } from '@/api'
+import { Spinner } from "../Spinner";
+import { WeatherContainer } from "../WeatherContainer";
+import { WeatherForecastResponse } from "@/types";
+import { fetchWeatherData } from "@/api";
 
 export const CityWeatherDetails = () => {
-	const [city, setCity] = useState('Ballia');
-	const [weatherData, setWeatherData] = useState<WeatherForecastResponse | null>(null);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState('');
-	
-	const handleFetchWeather = async () => {
-        if (!city) {
-            return;
-        }
-        setLoading(true);
-        setError('');
-        try {
-            const data = await fetchWeatherData(city);
-			console.log(data);
-            setWeatherData(data);
-        } catch (error) {
-            setError('Failed to fetch weather data');
-            setWeatherData(null); 
-        } finally {
-            setLoading(false);
-        }
-    };
+  const [city, setCity] = useState("Ballia");
+  const [weatherData, setWeatherData] =useState < WeatherForecastResponse | null > (null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [unit, setUnit] = useState<'C' | 'F'>('C');
 
-	useEffect(() => {
-		handleFetchWeather()
-	}
-	, [city]);
-	
-	console.log(weatherData?.city?.name);
+  const handleFetchWeather = async () => {
+    if (!city) {
+      return;
+    }
+    setLoading(true);
+    setError("");
+    try {
+      const data = await fetchWeatherData(city);
+      setWeatherData(data);
+    } catch (error) {
+      setError("Failed to fetch weather data");
+      setWeatherData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	if (loading) {
-		return <Spinner />;
-	}
+  useEffect(() => {
+    handleFetchWeather();
+  }, [city]);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   const firstData = weatherData?.list[0];
-  let dayOfWeek = '';
-  let formattedDate = '';
+  let dayOfWeek = "";
+  let formattedDate = "";
   if (firstData) {
-    const date = parse(firstData.dt_txt, 'yyyy-MM-dd HH:mm:ss', new Date());
+    const date = parseISO(firstData.dt_txt);
     dayOfWeek = format(date, "EEEE");
     formattedDate = format(date, "dd.MM.yyyy");
   }
-	
+
+  // Convert temperature and wind speed based on selected unit
+  const temperature =
+    unit === "C"
+      ? kelvinToCelsius(firstData?.main?.temp ?? 273.15)
+      : kelvinToFahrenheit(firstData?.main?.temp ?? 273.15);
+  const windSpeed =
+    unit === "C"
+      ? convertWindSpeedToKmh(firstData?.wind.speed ?? 0)
+      : convertWindSpeedToMph(
+          convertWindSpeedToKmh(firstData?.wind.speed ?? 0),
+        );
+  const windSpeedUnit = unit === "C" ? "km/h" : "mph";
+
   return (
-	<main className="flex flex-col w-full max-w-7xl mx-auto px-3 pt-4 pb-10">
-		<section className="space-y-4">
-			<div className="space-y-2">
-				<h2 className="flex gap-1 text-2xl items-end">
-					<p className="text-2xl">{dayOfWeek}</p>
-					<p className="text-lg">({formattedDate})</p>
-				</h2>
-				<WeatherContainer className="gap-2 px-6 items-center">
-					<div className="flex flex-col px-4">
-						<span className="text-5xl ">{kelvinToCelsius(firstData?.main?.temp ?? 273.15) }°</span>
-						<p className="text-xs space-x-1 whitespace-nowrap">
-							<span>Feels like </span>
-							<span>{kelvinToCelsius(firstData?.main?.feels_like ?? 273.15)}°</span>
-						</p>
-						<p className="text-xs space-x-2">
-						  	<span>{kelvinToCelsius(firstData?.main?.temp_min ?? 273.15)}°↓</span>
-							<span>{kelvinToCelsius(firstData?.main?.temp_max ?? 273.15)}°↑</span>
-						</p>
-
-					</div>
-					<div className="text-xs mt-4 md:mt-0">
-						<p>Precipitation: {Math.round((firstData?.pop ?? 0) * 100)}%</p>
-						<p>Humidity: {firstData?.main.humidity ?? 0}%</p>
-						<p>Wind: {convertWindSpeedToKmh(firstData?.wind.speed ?? 0)} km/h</p>
-					</div>
-
-				</WeatherContainer>
-			</div>
-		</section>
-		<section className="flex flex-col w-full max-w-7xl mx-auto px-3 pt-4 pb-10">
-		</section>
-	</main>
-  )
-}
+    <main className="flex flex-col w-full max-w-7xl mx-auto px-3 pt-4 pb-10">
+      <section className="space-y-4">
+        <div className="space-y-2">
+          <h2 className="flex gap-1 text-2xl items-end">
+            <p className="text-2xl">{dayOfWeek}</p>
+            <p className="text-lg">({formattedDate})</p>
+          </h2>
+          <WeatherContainer className="gap-2 px-6 items-center">
+            <div className="flex flex-col px-4">
+              <div className="flex items-start">
+                <span className="text-5xl ">{temperature.toFixed(0)}</span>
+                <span
+                  className={`cursor-pointer mx-2 ${unit === "C" ? "text-black" : "text-gray-400"}`}
+                  onClick={() => setUnit("C")}
+                >
+                  °C
+                </span>
+                |
+                <span
+                  className={`cursor-pointer mx-2 ${unit === "F" ? "text-black" : "text-gray-400"}`}
+                  onClick={() => setUnit("F")}
+                >
+                  °F
+                </span>
+              </div>
+              <p className="text-xs space-x-1 whitespace-nowrap">
+                <span>Feels like </span>
+                <span>
+                  {kelvinToCelsius(firstData?.main?.feels_like ?? 273.15)}°
+                </span>
+              </p>
+              <p className="text-xs space-x-2">
+                <span>
+                  {kelvinToCelsius(firstData?.main?.temp_min ?? 273.15)}°↓
+                </span>
+                <span>
+                  {kelvinToCelsius(firstData?.main?.temp_max ?? 273.15)}°↑
+                </span>
+              </p>
+            </div>
+            <div className="text-xs mt-4 md:mt-0">
+              <p>Precipitation: {Math.round((firstData?.pop ?? 0) * 100)}%</p>
+              <p>Humidity: {firstData?.main.humidity ?? 0}%</p>
+              <p>
+                Wind: {windSpeed.toFixed(1)} {windSpeedUnit}
+              </p>
+            </div>
+          </WeatherContainer>
+        </div>
+      </section>
+    </main>
+  );
+};
--- src/utils/convertSpeed.ts
+++ src/utils/convertSpeed.ts
@@ -1,3 +1,7 @@
 export const convertWindSpeedToKmh = (speedInMetersPerSecond: number): number => {
   return Math.round(speedInMetersPerSecond * 3.6);
 }
+
+export const convertWindSpeedToMph = (speedInKmh: number): number => {
+  return Math.round(speedInKmh / 1.609); // 1 km/h ≈ 0.621371 mph
+};
